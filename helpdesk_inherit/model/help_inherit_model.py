e345691c402e0ee08bb41a8ededa3608b5287bbd
abihedia <51483423+abihedia@users.noreply.github.com>
2022-06-03 12:24:52 +0100

    Add files via upload

---
+++ helpdesk_inherit/model/help_inherit_model.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+
+
+class HelpdeskModelHerit(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    products_id = fields.Many2one('product.product', string='Article')
+    lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série', help="Lot/Serial number", domain="[('product_id', '=', products_id)]")
+    tracking = fields.Selection(related='products_id.tracking')
