546eeb46dca6b89d704affd8abf3856f050f9378
abihedia <51483423+abihedia@users.noreply.github.com>
2022-06-03 13:27:11 +0100

    Update help_inherit_model.py

--- helpdesk_inherit/model/help_inherit_model.py
+++ helpdesk_inherit/model/help_inherit_model.py
@@ -4,8 +4,6 @@ from odoo import models, fields, api
 class HelpdeskModelHerit(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    products_id = fields.Many2one('product.product', string='Article')
-    lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série', help="Lot/Serial number", domain="[('product_id', '=', products_id)]")
-    tracking = fields.Selection(related='products_id.tracking')
-    product_serie_id = fields.Many2one('fleetserielarticle', string='Article Serie',domain="[('client_id', '=', partner_id)]")
+  
+    product_serie_id = fields.Many2one('fleetserielarticle', string='Article',domain="[('client_id', '=', partner_id)]")
 
