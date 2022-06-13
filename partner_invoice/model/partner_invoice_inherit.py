9333472805b6d84bc90831c20d6f629fe35b0e17
abihedia <51483423+abihedia@users.noreply.github.com>
2022-06-13 14:46:12 +0100

    Add files via upload

--- partner_invoice/model/partner_invoice_inherit.py
+++ partner_invoice/model/partner_invoice_inherit.py
@@ -8,16 +8,8 @@ class PartnerInvoiceHerit(models.Model):
     code_service = fields.Char('Code service')
     augmentation_sav = fields.Float(string='Augmentation SAV')
     augmentation_sav_bool= fields.Boolean(string="Augmentation SAV")
-    siret_customer = fields.Char('Siret')
 
 
-    @api.constrains('siret_customer')
-    def _check_phone_number(self):
 
-        for rec in self:
-            if rec.siret_customer and len(str(rec.siret_customer)) != 14 :
-                raise ValidationError(_("Wrong value enter"))
-            else:
-                return False
-        return {}
+
 
