770447980d34458b2aee23e59c4bed946778a731
abihedia <51483423+abihedia@users.noreply.github.com>
2022-06-29 15:20:12 +0100

    Delete factautomatique directory

--- factautomatique/models/liste_bon_fleet.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-from datetime import datetime
-
-class listbonfleet(models.Model):
-    _name = 'listboncommandefleet'
-    name = fields.Char(string='Bon de commande et matériels')
-    fleet_id = fields.Many2one('fleet.vehicle', string='les numéros de série')
-    devis_id = fields.Many2one('sale.order', string='Devis')
-
