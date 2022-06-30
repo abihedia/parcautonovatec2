35476c4dc8189f0c5185be3a3fd1a86e45c7ff27
abihedia <51483423+abihedia@users.noreply.github.com>
2022-06-30 12:07:13 +0100

    Update liste_bon_fleet.py

--- factautomatique/models/liste_bon_fleet.py
+++ factautomatique/models/liste_bon_fleet.py
@@ -6,4 +6,19 @@ class listbonfleet(models.Model):
     name = fields.Char(string='Bon de commande et matériels')
     fleet_id = fields.Many2one('fleet.vehicle', string='les numéros de série')
     devis_id = fields.Many2one('sale.order', string='Devis')
+    
+    comp_couleur_diff = fields.Integer(string="Nombre de pages couleur à facturer", compute="compute_couleur_diff")
+    comp_noir_diff = fields.Integer(string="Nombre de pages noir à facturer",compute="compute_noir_diff")
+
+    @api.depends('fleet_id')
+    def compute_couleur_diff(self):
+        for rec in self:
+            rec.comp_couleur_diff = rec.fleet_id.comp_couleur_diff
+            print(rec.comp_couleur_diff)
+
+    @api.depends('fleet_id')
+    def compute_noir_diff(self):
+        for rec in self:
+            rec.comp_couleur_diff = rec.fleet_id.comp_noir_diff
+            print(rec.comp_couleur_diff)
 
